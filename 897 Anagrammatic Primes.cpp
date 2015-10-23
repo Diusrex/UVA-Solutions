dd8993d86572159e71c2ad5cdcc55ac706586f30
Morgan Redshaw <diusrex@gmail.com>
2015-10-23 16:40:07 -0600

    Update 897 Anagrammatic Primes.cpp

--- 897 Anagrammatic Primes.cpp
+++ 897 Anagrammatic Primes.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    // Fairly simple to pre-calculate
+    // Fairly simple to pre-calculate. Can just calculate all primes in a table, then check if all orderings of the digits are prime.
     int anagramic[] = {2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97, 113, 131, 199, 311, 337, 373, 733, 919, 991};
     int size = 22;
     
